868cf2f22fb525dcc938655ccccbeda183e71398
danielmusselwhite <daniel.l.musselwhite@gmail.com>
2023-08-29 19:30:53 +0100

    Added other schemas for the other sections to be added to the website

--- backend_sanity/schemas/index.ts
+++ backend_sanity/schemas/index.ts
@@ -1,4 +1,11 @@
+import about from './about'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
 import testimonials from './testimonials'
+import workExperience from './workExperience'
+import works from './works'
 
 
-export const schemaTypes = [testimonials]
+export const schemaTypes = [about, brands, contact, experiences, skills, testimonials, workExperience, works]
