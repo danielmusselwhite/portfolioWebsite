615f404e20b1c72c84ffe373be50fd733f1d344a
danielmusselwhite <daniel.l.musselwhite@gmail.com>
2023-12-28 20:45:11 +0000

    Created Header Container, with dynamic content to respond to resizes

--- frontend_react/src/constants/images.ts
+++ frontend_react/src/constants/images.ts
@@ -1,6 +1,15 @@
-// import the png image at location: ../assets/images/logo.png
 import logo from '../assets/images/logo.png';
+import profile from '../assets/images/profile.png';
+import circle from '../assets/images/circle.svg';
+import python from '../assets/images/python.png';
+import java from '../assets/images/java.png';
+import cs from '../assets/images/CS.png';
 
 export default{
-    logo
+    logo,
+    profile,
+    circle,
+    python,
+    java,
+    cs
 };
--- frontend_react/src/container/Header/Header.tsx
+++ frontend_react/src/container/Header/Header.tsx
@@ -1,13 +1,83 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
+import {arrayOfSections} from '../../utils/utils';
+import { images } from '../../constants';
 import './Header.scss';
 
-const Header = () => {
-    return(
-        <div className="Header">
-            <h1>Header</h1>
-            <p>This is the Header page</p>
-        </div>
-    );
-}
+// scaleVariants constant used for animating the circles containing the programming languages
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
+
+// Header component
+const Header = () => (
+    <div id="home" className="app__header app__flex">
+
+        {/* Div animating from left to opaque, containing name + description badge */}
+        <motion.div
+            whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+            transition={{ duration: 0.8 }}
+            className="app__header-info"
+        >
+            <div className="app__header-badge">
+            <div className="badge-cmp app__flex">
+                <span>👋</span>
+                <div style={{ marginLeft: 20 }}>
+                <p className="p-text">Hello, I am</p>
+                <h1 className="head-text">Daniel</h1>
+                </div>
+            </div>
+
+            <div className="tag-cmp app__flex">
+                <p className="p-text">Software Engineer</p>
+                <p className="p-text">FullStack Developer</p>
+                <p className="p-text">BackEnd Specialist</p>
+            </div>
+            </div>
+        </motion.div>
+
+
+
+        {/* motion div fading into view, containing profile image + circle */}
+        {/* children of div will fade in after a further delay */}
+        <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.8, delayChildren: 0.8 }}
+            className="app__header-img"
+        >
+            <img src={images.profile} alt="profile_bg" />
+            <motion.img
+            whileInView={{ scale: [0, 1] }}
+            transition={{ duration: 1.2, ease: 'easeInOut' }}
+            src={images.circle}
+            alt="profile_circle"
+            className="overlay_circle"
+            />
+        </motion.div>
+
+
+        {/* motion div containing the circles of programming languages I have most commonly used */}
+        <motion.div
+            variants={scaleVariants}
+            whileInView={scaleVariants.whileInView}
+            className="app__header-circles"
+        >
+            {/* use map lambda function to iterate through array of images, and display each image in a div with a circular background */}
+            {[images.python, images.cs, images.java].map((circle, index) => (
+            <div className="circle-cmp app__flex" key={`circle-${index}`}>
+                <img src={circle} alt="profile_bg" />
+            </div>
+            ))}
+        </motion.div>
+    </div>
+);
+
 export default Header;
