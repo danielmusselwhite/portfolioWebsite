7ae29d98dce6f28df401be50ec946b5a373496cc
danielmusselwhite <daniel.l.musselwhite@gmail.com>
2023-12-29 15:27:26 +0000

    Create About Page with dummy data

--- frontend_react/src/constants/images.ts
+++ frontend_react/src/constants/images.ts
@@ -4,6 +4,11 @@ import circle from '../assets/images/circle.svg';
 import python from '../assets/images/python.png';
 import java from '../assets/images/java.png';
 import cs from '../assets/images/CS.png';
+import uon from '../assets/images/UoN.png';
+import ucl from '../assets/images/UCL.png';
+import backend from '../assets/images/backend.png';
+import frontend from '../assets/images/frontend.png';
+import cloud from '../assets/images/cloud.png';
 
 export default{
     logo,
@@ -11,5 +16,10 @@ export default{
     circle,
     python,
     java,
-    cs
+    cs,
+    uon,
+    ucl,
+    backend,
+    frontend,
+    cloud
 };
--- frontend_react/src/container/About/About.tsx
+++ frontend_react/src/container/About/About.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import { motion } from 'framer-motion';
+
+import images from '../../constants/images';
 
 import './About.scss';
 
+const abouts = [
+    { title: "Master of Science in Software Systems Engineering", description: "Talk about my degree from University College London", imgUrl: images.ucl},
+    { title: "Bachelor of Science in Computer Science Artificeial Intlligence", description: "Talk about my degree from University of Nottingham", imgUrl: images.uon},
+    { title: "Backend Development", description: "Talk abouut technologies I am experienced in and give examples of some backend related projects from uni + work + personal", imgUrl: images.backend},
+    { title: "Frontend Development", description: "Talk abouut technologies I am experienced in and give examples of uni + work + personal", imgUrl: images.frontend},
+    { title: "Cloud Development", description: "Talk abouut technologies I am experienced in and give examples of some projects from uni + work + personal", imgUrl: images.cloud}
+]
+
 const About = () => {
     return(
-        <div className="About">
-            <h1>About</h1>
-            <p>This is the about page</p>
+        <div id="about">
+            <h2 className="head-text">
+                Inspired To Build <span>Innovative</span> Solutions To <span>Your</span> Problems
+            </h2>
+
+            <div className="app__profiles">
+                {/* for each about in the abouts array of objects, create and populate a motion div */}
+                {abouts.map((about,index) => (
+                    <motion.div 
+
+                        
+
+                        whileInView={{ opacity: [0,1],
+                        scale: [1.1,1] }}
+                        whileHover={{ scale: 1.1 }}
+                        transition={{ duration: 0.5, type: 'tween' }}
+                        className="app__profile-item"
+                        key={about.title + index}
+                    >
+                        <img src={about.imgUrl} alt={about.title} />
+                        <h2 className="bold-text" style={{ marginTop: 20}}>{about.title}</h2>
+                        <p className="p-text" style={{marginTop: 10}}>{about.description}</p>
+                    </motion.div>
+                ))}
+            </div>
         </div>
     );
 }
