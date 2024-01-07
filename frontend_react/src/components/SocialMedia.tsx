0752d5b7d6a5ad8f04e0e9b6c1915015edb32d4a
danielmusselwhite <daniel.l.musselwhite@gmail.com>
2024-01-07 21:47:02 +0000

    Added links to the socials

--- frontend_react/src/components/SocialMedia.tsx
+++ frontend_react/src/components/SocialMedia.tsx
@@ -3,12 +3,16 @@ import { BsGithub, BsLinkedin } from 'react-icons/bs';
 const SocialMedia = () => {
   return (
     <div className="social-media">
-        <div>
-            <BsGithub></BsGithub>
-        </div>
-        <div>
-            <BsLinkedin></BsLinkedin>
-        </div>
+      <div>
+        <a href={"https://github.com/danielmusselwhite/"} target="_blank" rel="noopener noreferrer">
+          <BsGithub />
+        </a>
+      </div>
+      <div>
+        <a href={"https://www.linkedin.com/in/daniel-musselwhite/"} target="_blank" rel="noopener noreferrer">
+          <BsLinkedin />
+        </a>
+      </div>
     </div>
   );
 };
